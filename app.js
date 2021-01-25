035e424c8bb265328311b9fa1a17fbf68e18f2d3
Shahed1998 <shahedchowdhury120@gmail.com>
2021-01-26 01:59:46 +0600

    2nd commit

--- app.js
+++ app.js
@@ -3,14 +3,32 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  const today = new Date();
-  console.log(today.getDay());
-  if (today.getDay() === 5 || today.getDay() === 6) {
-    res.send('Holiday!');
+  const newDate = new Date();
+  const getDay = newDate.getDay();
+  const weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const today = weekDays[getDay];
+  let infoHalf = `Today is ${today} , `;
+  let fullInfo = '';
+
+  if (today === 'Friday' || today === 'Saturday') {
+    fullInfo = `${infoHalf}  it's a holiday`;
   } else {
-    res.send('Workdays');
+    fullInfo = `${infoHalf} it's a working day`;
   }
+
+  res.render('lists', { today: fullInfo });
 });
 
 app.listen(process.env.PORT || port, () => {
